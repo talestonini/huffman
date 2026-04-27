4f7934105e6127356f6706bb4cc41a93f923c54a
Tales Tonini <6622432+talestonini@users.noreply.github.com>
2026-04-27 11:07:38 +1000

    One more test

--- test/manual.sh
+++ test/manual.sh
@@ -10,6 +10,10 @@ cabal run tt-huffman decode test/resources/pnp.txt
 cabal run tt-huffman encode test/resources/sample_unicode.txt
 cabal run tt-huffman decode test/resources/sample_unicode.txt
 
+cabal run tt-huffman encode test/resources/ldv.txt
+cabal run tt-huffman decode test/resources/ldv.txt
+
 diff test/resources/tales.txt test/resources/tales.txt-inflated
 diff test/resources/pnp.txt test/resources/pnp.txt-inflated
 diff test/resources/sample_unicode.txt test/resources/sample_unicode.txt-inflated
+diff test/resources/ldv.txt test/resources/ldv.txt-inflated
